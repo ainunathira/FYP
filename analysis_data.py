29277df1860a8905bb66eb5ed5163153e361941b
ainunathira <93848349+ainunathira@users.noreply.github.com>
2024-07-02 03:17:01 +0800

    Update analysis_data.py

--- analysis_data.py
+++ analysis_data.py
@@ -35,9 +35,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-!pip install pandas_datareader
-!pip install -q yfinance
-%matplotlib inline
 
 sns.set_style('whitegrid')
 plt.style.use("fivethirtyeight")
