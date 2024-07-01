59ecb9b075d28c0e9c2b9117233d7080ad4e3f67
ainunathira <93848349+ainunathira@users.noreply.github.com>
2024-07-02 03:16:34 +0800

    Update analysis_data.py

--- analysis_data.py
+++ analysis_data.py
@@ -26,3 +26,51 @@ plt.plot(df2)
 plt.show()
 st.pyplot(plt) 
 st.write("Raw data:", df)
+
+##############################################################################################
+##What was the change in price of the stock overtime?
+##stock information with pandas, and how to analyze basic attributes of a stock.
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+!pip install pandas_datareader
+!pip install -q yfinance
+%matplotlib inline
+
+sns.set_style('whitegrid')
+plt.style.use("fivethirtyeight")
+
+# For reading stock data from yahoo
+from pandas_datareader.data import DataReader
+import yfinance as yf
+from pandas_datareader import data as pdr
+
+yf.pdr_override()
+
+# For time stamps
+from datetime import datetime
+
+# The tech stocks we'll use for this analysis
+tech_list = ['MLYBY']
+
+# Set up End and Start times for data grab
+tech_list = ['MLYBY']
+
+end = datetime.now()
+start = datetime(end.year - 1, end.month, end.day)
+
+for stock in tech_list:
+    globals()[stock] = yf.download(stock, start, end)
+    
+
+company_list = [MLYBY]
+company_name = ["MLYBY"]
+
+for company, com_name in zip(company_list, company_name):
+    company["company_name"] = com_name
+    
+df = pd.concat(company_list, axis=0)
+df.tail(20)
+st.write(data)
